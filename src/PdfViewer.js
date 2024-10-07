10c184f5c0139f1b496b598c8f3e670abde55234
Aditya Bhatt <aditya.bhatt@kocharsoft.in>
2024-10-07 19:23:35 +0530

    error solved

--- src/PdfViewer.js
+++ src/PdfViewer.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const PdfViewer = ({ pdfUrl, width, height }) => {
+const PdfViewer = ({ pdfUrl, styles }) => {
   const viewerUrl = `/pdfjs/web/viewer.html?file=${encodeURIComponent(pdfUrl)}`;
   return (
       <iframe
         title="PDF Viewer"
         src={viewerUrl}
-        width={width}
-        height={height}
+        style={styles}
       ></iframe>
   );
-};
-
-PdfViewer.propTypes = {
-  pdfUrl: PropTypes.string.isRequired,
-  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 export default PdfViewer;
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,42 @@
-const path = require("path");
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve("build"),
-    filename: "index.js",
-    libraryTarget: "commonjs2"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      }
-    ]
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/pdfjs'), // Path to your pdfjs folder in the package
+          to: path.resolve(__dirname, 'dist/public/pdfjs'), // Path where you want to copy it in the user's project
+        },
+      ],
+    }),
+  ],
   externals: {
-    react: "react"
-  }
+    react: 'react',
+    'react-dom': 'react-dom',
+  },
 };
